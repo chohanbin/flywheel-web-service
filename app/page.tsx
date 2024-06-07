b08602321206187f0c0ef739ecd4a5aff38c1927
Hanbin Cho <mraerok@gmail.com>
2024-06-07 06:50:05 -0400

    In the landing page, prepend Flywill title with Enso symbol

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import Link from "next/link";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
 import FlywheelTitle from "@/app/ui/flywheel-title";
+import FlywheelLogo from "./ui/flywheel-logo";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-6 bg-white">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {<FlywheelTitle />}
+        {<FlywheelLogo />}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
