a0855f46b724ed2f15fe2ef710a7dbc22a9ef0b6
Hanbin Cho <mraerok@gmail.com>
2024-06-09 15:50:30 -0400

    Ensure that the stock symbols are labeled

--- app/ui/dashboard/txn-history-table.tsx
+++ app/ui/dashboard/txn-history-table.tsx
@@ -32,7 +32,13 @@ export const nasdaqStockSummaryBasepath =
   "https://www.nasdaq.com/market-activity/stocks/";
 export function StockSymbol({ symbol }: { symbol: string }) {
   const link = `${nasdaqStockSummaryBasepath}${symbol}`;
-  return <a href={`${link}`}>{symbol}</a>;
+  return (
+    <div className="underline text-blue-600 hover:text-blue-800">
+      <a target="_blank" rel="noreferrer noopener" href={`${link}`}>
+        {symbol}
+      </a>
+    </div>
+  );
 }
 
 export function formatDisplayValue(key: string, value: string | number) {
